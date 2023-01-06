23b5ff4def8ee008a8838150f31f36908242e316
Ahmet Kemal Gaya <49936571+conokemal@users.noreply.github.com>
2023-01-06 12:37:31 +0300

    DBScript

---
+++ RoadsApiDemo/Controllers/GMapExamplesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RoadsApiDemo.Controllers
+{
+    public class GMapExamplesController : Controller
+    {
+        // GET: GMapExamples
+        public ActionResult RoadsApiDemo()
+        {
+            return View();
+        }
+    }
+}
